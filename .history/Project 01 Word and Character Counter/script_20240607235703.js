f88d39c2a81adddbf3253e924247d1b898036826
Sumit Kumar Garssa <95330561+sumitkumargarssa@users.noreply.github.com>
2024-06-07 23:58:58 +0530

    Delete .history/Project 01 Word and Character Counter directory

--- .history/Project 01 Word and Character Counter/script_20240607235703.js
+++
@@ -1,9 +0,0 @@
-let inputTextArea = document.getElementById("input-textarea");
-let characCount = document.getElementById("charac-count");
-let wordCount = document.getElementById("word-count");
-
-inputTextArea.addEventListener("input", () => {
-  characCount.textContent = inputTextArea.value.length;
-  let txt = inputTextArea.value.trim();
-  wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
-});
